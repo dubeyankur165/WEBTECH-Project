3a38b847650ef74bee2289df5156181c95b08ae4
dubeyankur165 <90439081+dubeyankur165@users.noreply.github.com>
2022-01-14 13:17:08 +0530

    Add files via upload

---
+++ backend/models/Product.js
@@ -0,0 +1,30 @@
+//Products
+
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:{
+    type: String,
+    required:true
+    },
+    desciption:{
+        type: String,
+        required:true
+    },
+    price:{
+        type: Number,
+        required:true
+    },
+    countInstock:{
+        type: Number,
+        required:true
+    },
+    imageUrl:{
+        type: String,
+        required:true
+    }
+})
+
+const Product = mongoose.model('product', productSchema);
+
+module.exports = Product;
